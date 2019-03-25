2f8603c3b32abe117435adfd0897cec2454ad446
ozwaldo <ozwaldo@gmail.com>
2019-03-24 23:37:51 -0600

    Add files via upload
    Ok

---
+++ script.js
@@ -0,0 +1,18 @@
+var example2 = new Vue({
+    el: '#frutas',
+    data: {
+      parentMessage1: 'Amarillas',
+      items1: [
+        { message: 'Platano' },
+        { message: 'Mango' },
+     
+      ],
+      parentMessage2: 'Verdes',
+      items2: [
+        { message: 'Sandia' },
+        { message: 'Pepino' },
+     
+      ]
+    }
+  }
+)
